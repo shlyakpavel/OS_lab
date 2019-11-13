9344e92fd3a135e2c1898496623f6b96ff400cb6
Pavel Shlyak <shlyak-pavel2011@ya.ru>
2019-11-13 17:55:55 +0300

    Filter 1

--- Lab4/filter1.c
+++ Lab4/filter1.c
@@ -1,47 +1,21 @@
-#include <unistd.h>
+// Разработать фильтр, ограничивающий длину каждого слова в строке 5 символами.
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 
-int main(void)
-/*  Создание программного канала    */
-/*  для команд ls и wc                 */
+#define MAX_LENGHT 5
+
+int main()
 {
-    int pid, pid2;
-    int fd[2];
-    int status, dead;
-    switch(pid = fork())
-    {
-        case -1: /* Cбой при вызове fork()  */
-            printf("Ошибка при вызове fork() #1 \n");
-            exit(1);
-            break;
-        case 0:  /*  ПОТОМОК #1 */
-            pipe(fd);
-            
-            switch(pid2 = fork())
-            {
-                case -1: /* Cбой при вызове fork()  */
-                    printf("Ошибка при вызове fork() #2 \n");
-                    exit(2);
-                    break;
-                case 0:        /*  ПОТОМОК  #2 */
-                    close(0); dup(fd[0]); close(fd[0]); close(fd[1]);
-                    execl("/usr/bin/wc", "wc", NULL);
-                    puts("Ошибка при вызове WC \n");
-                    exit(-1);
-                    break;
-                default: /*                     */
-                    close(1); dup(fd[1]);
-                    close(fd[1]); close(fd[0]);
-                    execl("/bin/ls", "ls", NULL);
-                    break;
-            }
-            puts("Ошибка при вызове LS\n");
-            exit(-1);
-        default: /*  ПРЕДОК ГЛАВНЫЙ  */
-            dead = wait(&status);
-            exit(0);
-            break;
+    char c;
+    int cnt = 0;
+    while ((c = getchar()) != EOF){
+        if (c == ' ')
+            cnt = 0;
+        else
+            cnt++;
+        if (cnt <= MAX_LENGHT)
+            putchar(c);
     }
+    //putchar(EOF);
 }
